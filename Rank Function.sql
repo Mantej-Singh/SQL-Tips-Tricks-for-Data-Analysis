28218c00d0413a2a15e566c5fd43395a627a19b6
Mantej Singh Dhanjal <mdmantejsingh@gmail.com>
2017-01-25 16:56:17 -0500

    Rank for top products by each subcategory

--- Rank Function.sql
+++ Rank Function.sql
@@ -4,20 +4,19 @@ use AdventureWorksDW2014;
 --Find the top products of 2013
 
 SELECT 
-		RANK() OVER (ORDER BY sum(s.SalesAmount) DESC) as 'Rank'
-	,	count(DISTINCT s.SalesOrderNumber) 'OrderCount' -- use 1 instead of a field for faster performance
-	,	sum(s.SalesAmount) 'TotalSales'
-	,	cat.EnglishProductCategoryName 'Category'
-    ,	sub.EnglishProductSubcategoryName 'SubCategory'	
+		RANK() OVER (ORDER BY sum(s.SalesAmount) DESC) as 'Rank'--You can use ROW_NUMBER() also
+	,	count(DISTINCT s.SalesOrderNumber) as 'OrderCount'
+	,	sum(s.SalesAmount) as 'TotalSales'
+	,	cat.EnglishProductCategoryName as 'Category'
+    ,	sub.EnglishProductSubcategoryName as 'SubCategory'	
 FROM FactInternetSales s
 INNER JOIN DimProduct p ON s.ProductKey = p.ProductKey
 INNER JOIN DimProductSubcategory sub ON p.ProductSubcategoryKey = sub.ProductSubcategoryKey
 INNER JOIN DimProductCategory cat ON sub.ProductCategoryKey = cat.ProductCategoryKey
--- filter
-WHERE YEAR(s.OrderDate) = 2013 --use date function to parse year
--- must use group by in order for aggregation to work properly
+-- filter on Year 2013
+WHERE YEAR(s.OrderDate) = 2013
 GROUP BY
-		cat.EnglishProductCategoryName -- column aliases aren't allowed
+	cat.EnglishProductCategoryName 
     ,	sub.EnglishProductSubcategoryName	
 
-ORDER BY 1;
+ORDER BY 1;--order by RANK()
